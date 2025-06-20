d5e6b560e7b83d24d5819a4097381e40008ae655
Mark Zorikhin <hnau256@gmail.com>
2025-06-20 10:27:01 +0300

    inc versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    val kotlinVersion = "2.1.10"
-    id("androidx.room") version "2.6.1"
-    id("com.android.application") version "8.9.0"
+    val kotlinVersion = "2.1.20"
+    id("androidx.room") version "2.7.2"
+    id("com.android.application") version "8.9.3"
     id("org.jetbrains.kotlin.plugin.compose") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.serialization") version kotlinVersion
-    id("com.google.devtools.ksp") version "2.1.10-1.0.31"
+    id("com.google.devtools.ksp") version "2.1.20-1.0.31"
     kotlin("android") version kotlinVersion
 }
 
@@ -55,23 +55,23 @@ room {
 }
 
 dependencies {
-    implementation("androidx.compose.material3:material3:1.3.1")
+    implementation("androidx.compose.material3:material3:1.3.2")
     implementation("androidx.activity:activity-compose:1.10.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
-    implementation("io.arrow-kt:arrow-core:1.2.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
+    implementation("io.arrow-kt:arrow-core:1.2.4")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.2")
-    implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
-    implementation("org.slf4j:slf4j-simple:2.0.7")
-    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.8")
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
+    implementation("org.slf4j:slf4j-simple:2.0.17")
+    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.4.0")
 
-    val composeUi = "1.7.8"
+    val composeUi = "1.8.3"
     implementation("androidx.compose.ui:ui:$composeUi")
     implementation("androidx.compose.ui:ui-tooling:$composeUi")
     debugImplementation("androidx.compose.ui:ui-tooling-preview:$composeUi")
     implementation("androidx.compose.foundation:foundation:$composeUi")
-    implementation("androidx.compose.material:material-icons-extended:$composeUi")
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
 
-    val room = "2.6.1"
+    val room = "2.7.2"
     implementation("androidx.room:room-runtime:$room")
     implementation("androidx.room:room-ktx:$room")
     ksp("androidx.room:room-compiler:$room")
